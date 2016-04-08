d9aebc15e65913f7434c8fef01a5f354cb91c1db
cbenitez <cristhian.benitez5@gmail.com>
2016-04-08 15:13:37 -0400

    Creacion de Login
    Signed-off-by: cbenitez <cristhian.benitez5@gmail.com>

---
+++ is2/autenticacion/urls.py
@@ -0,0 +1,17 @@
+from django.conf.urls import url
+
+from . import views
+from django.contrib.auth.views import logout
+
+urlpatterns = [
+    url(r'^login/$', views.login_view, name='login_view'),
+    url(r'^$', views.login_view, name='login_view'),
+     url(r'^auth_view/$', views.auth_view, name='auth_view'),
+    url(r'^logout/$', logout, {'next_page': '/'}, name='logout_view'),
+    url(r'^loggedin/$', views.loggedin, name='loggedin'),
+    url(r'^invalid/$', views.invalid, name='invalid'),
+
+
+]
+
+
--- is2/autenticacion/views.py
+++ is2/autenticacion/views.py
@@ -1,3 +1,43 @@
-from django.shortcuts import render
+from django.shortcuts import render_to_response
+from django.shortcuts import HttpResponsePermanentRedirect
+from django.contrib.auth import authenticate, login
+from django.template.context_processors import csrf
 
 # Create your views here.
+
+
+def login_view(request):
+    if request.user.is_authenticated():
+        return HttpResponsePermanentRedirect('/loggedin')
+    else:
+        c = {}
+        c.update(csrf(request))
+        return render_to_response('autenticacion/login_view.html', c)
+
+def auth_view(request):
+    username = request.POST.get('username', '')
+    password = request.POST.get('password', '')
+    user = authenticate(username=username, password=password)
+
+    if user is not None:
+        login(request, user)
+        return HttpResponsePermanentRedirect('/loggedin')
+    else:
+        return HttpResponsePermanentRedirect('/invalid')
+
+def loggedin(request):
+     if request.user.is_authenticated():
+         return render_to_response('autenticacion/loggedin.html', {'full_name': request.user.username})
+     else:
+         return HttpResponsePermanentRedirect('/login')
+
+def invalid(request):
+    return render_to_response('autenticacion/invalid.html')
+
+
+
+
+
+
+
+
