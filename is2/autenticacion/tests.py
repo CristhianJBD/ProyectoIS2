67615a985f928df4f0a27f82512fba266c519e8d
ChristianDelg <ing.delgadomontiel@gmail.com>
2016-04-08 22:06:02 -0400

    pruebas de test
    Signed-off-by: ChristianDelg <ing.delgadomontiel@gmail.com>

--- is2/autenticacion/tests.py
+++ is2/autenticacion/tests.py
@@ -1,4 +1,3 @@
-from django.contrib.auth.models import User
 from django.test import TestCase, RequestFactory
 from django.test import Client
 # Create your tests here.
@@ -11,34 +10,33 @@ class test(TestCase):
 
     def test_login(self):
         """
-        Response recibira un codigo de estado.
-
-         200 códigos de estado de la respuesta
-        :return:
+         el test muestra que al dirigir a la pagina login, este envia un response, con codigo
+         200, que significa que la pagina cargo correctamente
         """
         response = self.client.get('/login/')
         self.assertEquals(response.status_code, 200)
         self.assertNotAlmostEquals(response.status_code, 301)
 
-    def test_loggedin(self):
-        """
-        301 Movido permanentemente se utiliza para el permanente cambio de dirección URL
-        :return:
-        """
-        u = User()
-        u.username = 'admin'
-        u.set_password('adminadmin')
-        u.is_active = True
-        u.save()
-        self.client.post('/login/', {'username': 'admin', 'password': 'adminadmin'})
-        response = self.client.get('/loggedin/')
-        self.assertEquals(response.status_code, 301)
-
-    def test_invalid(self):
-        c = Client()
-        response = c.post('/login/', {'username': 'admin', 'password': 'adminadmin'})
-        self.assertEquals(response.status_code, 200)
 
     def test_ver_loginPage(self):
+        """
+         el test muestra que al dirigir a la pagina login, este envia un response, con el template
+        """
         response = self.client.get('/login/')
         self.assertTemplateUsed(response, 'autenticacion/login_view.html')
+
+
+    def test_logout(self):
+         """
+        indica que que la redireccion es temporal por lo tanto envia un codigo 302
+        """
+         resp = self.client.get('/logout/')
+         self.assertEqual(resp.status_code, 302)
+
+
+    def test_loggedin(self):
+        """
+        indica que que la redireccion es permante por lo tanto envia un codigo 301
+        """
+        response = self.client.get('/loggedin/')
+        self.assertEquals(response.status_code, 301)
